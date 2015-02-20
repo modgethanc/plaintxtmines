0fc8bfab49d86f92d2472cf0bb3c47640cb343a5
endorphant <endorphant@tilde.town>
2015-02-20 15:19:43 -0500

    cleaned up mine creation

--- bin/mines.py
+++ bin/mines.py
@@ -3,43 +3,44 @@
 import random
 
 def generate(minerates):
-    rates = open(minerates,'r')
-    tilde = int(rates.readline().rstrip('\r\n'))
-    pound = int(rates.readline().rstrip('\r\n'))
-    spiral = int(rates.readline().rstrip('\r\n'))
-    amper = int(rates.readline().rstrip('\r\n'))
-    splat = int(rates.readline().rstrip('\r\n'))
-    lbrack = int(rates.readline().rstrip('\r\n'))
-    rbrack = int(rates.readline().rstrip('\r\n'))
-    carat = int(rates.readline().rstrip('\r\n'))
-    bound = int(rates.readline().rstrip('\r\n'))
+    ratefile = open(minerates,'r')
+    rates = []
+    for x in ratefile:
+        rates.append(int(x.rstrip()))
+
+    bound = rates[8]
 
     seed = random.randrange(1,bound)
-    tildes = seed * tilde + random.randrange(1,bound)
-    pounds = seed * pound + random.randrange(1,bound)
-    spirals = seed * spiral + random.randrange(1,bound)
-    ampers = seed * amper + random.randrange(1,bound)
-    splats = seed * splat + random.randrange(1,bound)
-    lbracks = seed * lbrack + random.randrange(1,bound)
-    rbracks = seed * rbrack + random.randrange(1,bound)
-    carats = seed * carat + random.randrange(1,bound)
 
-    total = tildes + pounds + spirals + ampers + splats + lbracks + rbracks + carats
+    resources = [0,0,0,0,0,0,0,0]
+    total = 0
 
-    print "tildes:  %d (%d)" % (tildes, 100*float(tildes)/float(total))
-    print "pounds:  %d (%d)" % (pounds, 100*float(pounds)/float(total))
-    print "spirals: %d (%d)" % (spirals, 100*float(spirals)/float(total))
-    print "ampers:  %d (%d)" % (ampers, 100*float(ampers)/float(total))
-    print "splats:  %d (%d)" % (splats, 100*float(splats)/float(total))
-    print "lbracks: %d (%d)" % (lbracks, 100*float(lbracks)/float(total))
-    print "rbracks: %d (%d)" % (rbracks, 100*float(rbracks)/float(total))
-    print "carats:  %d (%d)" % (carats, 100*float(carats)/float(total))
+    i = 0
+
+    for x in resources:
+        resources[i] = seed * rates[i] + random.randrange(1,bound)
+        total += resources[i]
+        i += 1
+
+    print "seed: %d" % (seed)
+    print "tildes:  %d (%d%%)" % (resources[0], 100*float(resources[0])/float(total))
+    print "pounds:  %d (%d%%)" % (resources[1], 100*float(resources[1])/float(total))
+    print "spirals: %d (%d%%)" % (resources[2], 100*float(resources[2])/float(total))
+    print "ampers:  %d (%d%%)" % (resources[3], 100*float(resources[3])/float(total))
+    print "splats:  %d (%d%%)" % (resources[4], 100*float(resources[4])/float(total))
+    print "lbracks: %d (%d%%)" % (resources[5], 100*float(resources[5])/float(total))
+    print "rbracks: %d (%d%%)" % (resources[6], 100*float(resources[6])/float(total))
+    print "carats:  %d (%d%%)" % (resources[7], 100*float(resources[7])/float(total))
     print "\ntotal: %d" % (total)
 
 
 #testing below
 
+print "MINE #1"
 generate("standardrates")
+print "\nMINE #2"
 generate("standardrates")
+print "\nMINE #3"
 generate("standardrates")
+print "\nMINE #4"
 generate("standardrates")
