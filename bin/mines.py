55dcb5059be217e65e62c035a372924131146795
endorphant <endorphant@tilde.town>
2015-02-20 15:41:20 -0500

    opening recordfiles

--- bin/mines.py
+++ bin/mines.py
@@ -24,34 +24,43 @@ def generate(minerates): #takes a minerate file; returns a list of resource amou
 
     return resources
 
-def writeMine(mine, record):
+def writeMine(mine, record): #returns a filename for a list of resoureces recorded
     recordfile = open(record, 'w')
     for x in mine:
         recordfile.write(str(x))
         recordfile.write("\n")
 
     recordfile.close()
+    return record
+
+def openMine(record): #returns a list of resources when given a file
+    recordfile = open(record, 'r')
+    mine = []
+    for x in recordfile:
+        mine.append(x)
+
+    return mine
 
 def printMine(mine):
     total = 0
     for x in mine:
-        total += x
+        total += int(x)
 
-    print "tildes:  %d (%d%%)" % (mine[0], 100*float(mine[0])/float(total))
-    print "pounds:  %d (%d%%)" % (mine[1], 100*float(mine[1])/float(total))
-    print "spirals: %d (%d%%)" % (mine[2], 100*float(mine[2])/float(total))
-    print "ampers:  %d (%d%%)" % (mine[3], 100*float(mine[3])/float(total))
-    print "splats:  %d (%d%%)" % (mine[4], 100*float(mine[4])/float(total))
-    print "lbracks: %d (%d%%)" % (mine[5], 100*float(mine[5])/float(total))
-    print "rbracks: %d (%d%%)" % (mine[6], 100*float(mine[6])/float(total))
-    print "carats:  %d (%d%%)" % (mine[7], 100*float(mine[7])/float(total))
+    print "tildes:  %d (%d%%)" % (int(mine[0]), 100*float(mine[0])/float(total))
+    print "pounds:  %d (%d%%)" % (int(mine[1]), 100*float(mine[1])/float(total))
+    print "spirals: %d (%d%%)" % (int(mine[2]), 100*float(mine[2])/float(total))
+    print "ampers:  %d (%d%%)" % (int(mine[3]), 100*float(mine[3])/float(total))
+    print "splats:  %d (%d%%)" % (int(mine[4]), 100*float(mine[4])/float(total))
+    print "lbracks: %d (%d%%)" % (int(mine[5]), 100*float(mine[5])/float(total))
+    print "rbracks: %d (%d%%)" % (int(mine[6]), 100*float(mine[6])/float(total))
+    print "carats:  %d (%d%%)" % (int(mine[7]), 100*float(mine[7])/float(total))
     print "\ntotal: %d" % (total)
 
     return mine
 
 #testing below
 print "MINE #1"
-writeMine(printMine(generate("standardrates")), "m1")
+printMine(openMine(writeMine(generate("standardrates"), "m1")))
 print "\nMINE #2"
 writeMine(printMine(generate("standardrates")), "m2")
 print "\nMINE #3"
