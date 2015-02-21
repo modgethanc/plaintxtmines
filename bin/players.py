4cb7f070594c716bc46c364b583fb29387965cb6
Vincent Zeng <hvincent@modgethanc.com>
2015-02-21 17:56:19 -0500

    adding mining

--- bin/players.py
+++ bin/players.py
@@ -16,24 +16,40 @@ def new(player):
 
     return player
 
-def newMine(player):
+def newMine(player, rates):
     playerfile = open('../data/'+player+'.player', 'r')
     playerdata = []
     
     for x in playerfile:
          playerdata.append(x)
 
-    minename = mines.writeMine(mines.generate("standardrates"))
-    minelist = playerdata[0].rstrip()
-    minelist += "," + minename + "\n"
-    playerdata[0] = minelist
+    minename = mines.writeMine(mines.generate(rates))
+    playerdata[0] = playerdata[0].rstrip() + minename + ",\n"
     playerfile.close()
 
     playerfile = open('../data/'+player+'.player', 'w')
     for x in playerdata:
         playerfile.write(x)
+    playerfile.close()
 
     return minename
 
-new("hvincent")
-print newMine("hvincent")
+def getMines(player):
+    playerfile = open('../data/'+player+'.player', 'r')
+    minelist = playerfile.readline().rstrip().split(',')
+    minelist.remove('')
+    playerfile.close()
+
+    return minelist
+
+def excavate(player, mine):
+    holdings = getMines(player)
+    if holdings.index(mine):
+        return mines.excavate(mine)
+    else:
+        return "that's not ur mine"
+
+#new("hvincent")
+#print newMine("hvincent", "standardrates")
+print getMines("hvincent")
+print excavate("hvincent", getMines("hvincent")[0])
