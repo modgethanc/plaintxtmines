00b7f29f4ae0b428a5d337b0fb78691d201f1451
Vincent Zeng <hvincent@modgethanc.com>
2015-02-21 12:57:04 -0500

    shortened mineprint

--- bin/mines.py
+++ bin/mines.py
@@ -25,7 +25,7 @@ def generate(minerates): #takes a minerate file; returns a list of resource amou
 
     return resources
 
-def writeMine(mine, record='../data/'+names.medium()+'.mine'): #returns a filename for a list of resources recorded
+def writeMine(mine, record='../data/'+names.medium()+'.mine'): #returns a mine name for a list of resources recorded
     recordfile = open(record, 'w')
     total = 0
     for x in mine:
@@ -35,35 +35,36 @@ def writeMine(mine, record='../data/'+names.medium()+'.mine'): #returns a filena
     
     recordfile.write(str(total))
     recordfile.close()
-    return record
 
-def openMine(record): #returns a list of resources when given a file
+    return record.split('/')[-1].split('.')[0]
+
+def openMine(record): #returns a list of mine data given a filename
     recordfile = open(record, 'r')
     mine = []
     for x in recordfile:
         mine.append(x)
+    
+    mine.append(record.split('/')[-1].split('.')[0])
 
     return mine
 
 def printMine(mine):
-    #total = 0
-    #for x in mine:
-    #    total += int(x)
-
-    print "tildes:  %d (%d%%)" % (int(mine[0]), 100*float(mine[0])/float(mine[8]))
-    print "pounds:  %d (%d%%)" % (int(mine[1]), 100*float(mine[1])/float(mine[8]))
-    print "spirals: %d (%d%%)" % (int(mine[2]), 100*float(mine[2])/float(mine[8]))
-    print "ampers:  %d (%d%%)" % (int(mine[3]), 100*float(mine[3])/float(mine[8]))
-    print "splats:  %d (%d%%)" % (int(mine[4]), 100*float(mine[4])/float(mine[8]))
-    print "lbracks: %d (%d%%)" % (int(mine[5]), 100*float(mine[5])/float(mine[8]))
-    print "rbracks: %d (%d%%)" % (int(mine[6]), 100*float(mine[6])/float(mine[8]))
-    print "carats:  %d (%d%%)" % (int(mine[7]), 100*float(mine[7])/float(mine[8]))
+    print mine[-1]
+    print "~ %d (%d%%)" % (int(mine[0]), 100*float(mine[0])/float(mine[8]))
+    print "# %d (%d%%)" % (int(mine[1]), 100*float(mine[1])/float(mine[8]))
+    print "@ %d (%d%%)" % (int(mine[2]), 100*float(mine[2])/float(mine[8]))
+    print "& %d (%d%%)" % (int(mine[3]), 100*float(mine[3])/float(mine[8]))
+    print "* %d (%d%%)" % (int(mine[4]), 100*float(mine[4])/float(mine[8]))
+    print "[ %d (%d%%)" % (int(mine[5]), 100*float(mine[5])/float(mine[8]))
+    print "] %d (%d%%)" % (int(mine[6]), 100*float(mine[6])/float(mine[8]))
+    print "^ %d (%d%%)" % (int(mine[7]), 100*float(mine[7])/float(mine[8]))
     print "\ntotal: %d" % (int(mine[8]))
 
     return mine
 
 #testing below
-print "MINE #1"
-printMine(openMine(writeMine(generate("standardrates"), "../data/m1")))
-print "\nNAMED MINE"
-printMine(openMine(writeMine(generate("standardrates"))))
+printMine(openMine('../data/'+writeMine(generate("standardrates"), "../data/m1")))
+print writeMine(generate("standardrates"), '../data/m2')
+printMine(openMine('../data/m2'))
+print "\n"
+printMine(openMine('../data/'+writeMine(generate("standardrates"))+'.mine'))
