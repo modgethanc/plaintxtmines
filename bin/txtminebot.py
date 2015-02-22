0a229e7bff4eb4709c437b20b8766e19d40b0078
Vincent Zeng <hvincent@modgethanc.com>
2015-02-22 12:56:41 -0500

    cleaning up irc entry

--- bin/txtminebot.py
+++ bin/txtminebot.py
@@ -6,6 +6,9 @@ import sys
 from optparse import OptionParser
 
 import formatter
+import mines
+import players
+import names
 
 parser = OptionParser()
 
@@ -15,27 +18,34 @@ parser.add_option("-n", "--nick", dest="nick", default='txtminebot', help="the n
 
 (options, args) = parser.parse_args()
 
+def ping():
+  ircsock.send("PONG :pingis\n")
+
 def joinchan(chan):
   ircsock.send("JOIN "+ chan +"\n")
 
 def connect(server, channel, botnick):
   ircsock.connect((server, 6667))
-  ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :hvincent\n") # user authentication
+  ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :hvincent\n") 
   ircsock.send("NICK "+ botnick +"\n")
 
   joinchan(channel)
 
+
+###########################
+
 def listen():
   while 1:
-    ircmsg = ircsock.recv(2048)
-    ircmsg = ircmsg.strip('\n\r')
+    msg = ircsock.recv(2048)
+    msg = ircmsg.strip('\n\r')
+
+    if ircmsg.find("PING :") != -1:
+      ping()
 
     formatted = formatter.format_message(ircmsg)
 
     if "" == formatted:
       continue
-
-    # print formatted
 
     split = formatted.split("\t")
     time = split[0]
@@ -44,6 +54,14 @@ def listen():
     channel = split[3]
     messageText = split[4]
 
+    if msg.find("!join") != -1:
+        #ircsock.send("PRIVMSG " + channel + " :" + user + ": k\n")
+        split = msg.split(" ");
+        for x in split:
+            if x.find("#") != -1:
+                joinchan(x)
+
+#########################
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connect(options.server, options.channel, options.nick)
 listen()
