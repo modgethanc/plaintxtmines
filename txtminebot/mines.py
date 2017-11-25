a96077df30134830b07cf8be597f627c6ad5b89b
Vincent Zeng <hvincent@modgethanc.com>
2017-11-25 00:24:47 -0500

    first pass roughing in mine classes

--- txtminebot/mines.py
+++ txtminebot/mines.py
@@ -1,45 +1,196 @@
 #!/usr/bin/python
 
+'''
+This contains the class and functions for mines.
+
+Mines contain veins of resources, and are generated on demand when a new mine is
+opened.
+
+By convention, we use reslist as an array of ints, each item corresponding to
+the number of resources assuming the following order:
+    reslist[0]: ~
+    reslist[1]: @
+    reslist[2]: #
+    reslist[3]: &
+    reslist[4]: *
+    reslist[5]: [
+    reslist[6]: ]
+    reslist[7]: ^
+
+Future versions should use a dict for resources.
+
+plaintxtmines is a text-based multiplayer mining simulator. For more
+information, see the full repository:
+
+https://github.com/modgethanc/plaintxtmines
+'''
+
+import os
 import random
 import gibber
-import os
 
-def newMine(owner, minerate="standardrates"):
-    minename = gibber.medium()
-    while os.path.isfile('../data/'+minename+'.mine'): # check for mine colision
+class Mine():
+    '''
+    Implements a mine object.
+    '''
+
+    def __init__(self):
+        '''
+        Initial mine conditions.
+        '''
+
+        ## given stats
+        self.startingRes = [0,0,0,0,0,0,0,0]
+        self.startingTotal = 0
+        self.owner = None
+        self.name = None
+
+        ## mutables
+        self.currentRes = [0,0,0,0,0,0,0,0]
+        self.currentTotal = 0
+        self.workers = []
+
+    def save(self):
+        '''
+        Write self to disk.
+        '''
+
+        pass
+
+    def load(self):
+        '''
+        Loads a file from disk.
+        '''
+        
+        pass
+
+    def create(self, player_input):
+        '''
+        Generate a new mine, assuming inputs from a player with optional
+        minerate. If no minerate is given, pass standard minerates down.
+
+        Mine name is created with the gibber function, and checks against all
+        previously created mines for name collisions.
+        '''
+
+        # generate name
         minename = gibber.medium()
+        while os.path.isfile('../data/'+minename+'.mine'): # check for mine colision
+            minename = gibber.medium()
 
-    res = generateRes(minerate)
-    total = sumRes(res)
+        reslist = roll_resources(minerate)
+        total = sum_resources(reslist)
 
-    j = ','
-    minefile = open('../data/'+minename+'.mine', 'w+')
-    minefile.write(j.join(res)+'\n') # 0 res counts
-    minefile.write(str(total)+'\n') # 1 original total
-    minefile.write(owner+'\n') # 2 owner
-    minefile.write('\n') # 3 workers
+        ## given stats
+        self.startingRes = reslist
+        self.startingTotal = total
+        self.owner = player_input.nick
 
-    minefile.close()
+        ## mutables
+        self.currentRes = reslist
+        self.currentTotal = total
+        self.workers = [player_input.nick]
 
-    return minename # for mine name confirmation
+    def excavate(self, rate=10, width=3):
+        '''
+        Given optional strike rate and width, return an excavation from this
+        mine. Those resources are moved from this mine's data.
+        '''
 
-def generateRes(minerate): #takes a minerate file; returns a list of resource amounts for a generated mine
+        excavated = [0,0,0,0,0,0,0,0]
+        reslist = self.currentRes
+
+        mineData = openMine(mine)
+        veins = []
+
+        if self.currentTotal <= rate:
+            # clear the mine right away if mine has less than strikerate
+
+            excavated = reslist
+            self.currentRes = [0,0,0,0,0,0,0,0]
+            self.save()
+            return excavated
+
+        i = width
+        mineChoices = [0,1,2,3,4,5,6,7]
+        while i > 0: # pick veins
+            vein = random.choice(mineChoices)
+            veins.append(vein)
+            mineChoices.remove(vein)
+            i -= 1
+
+        i = rate
+        veinsLeft = width
+        while i > 0:
+            # strike, and count res extracted
+            if veinsLeft > 0:
+                strike = random.choice(veins)
+            else:
+                break
+
+            if int(reslist[strike]) > excavated[strike]:
+                # check for remaining res
+                excavated[strike] += 1
+                i -= 1
+            else:
+                # stop striking empty vein
+                veins.remove(strike)
+                veinsLeft -= 1
+
+        i = 0
+        while i < 8:
+           # remove resources from mine
+           vein = int(reslist[i])
+           vein -= excavated[i]
+           reslist[i] = vein
+           i += 1
+
+        # record mine actions
+        self.currentRes = reslist
+        self.currentTotal = sum_resources(reslist)
+        self.save()
+
+        return excavated
+
+## mine helper functions
+
+def roll_resources(minerate):
+    '''
+    Takes a minerate file; returns a list of resource amounts for a
+    generated mine.
+    '''
+
+    # open minerate file and process
     ratefile = open(minerate,'r')
     rates = []
     for x in ratefile:
         rates.append(int(x.rstrip()))
     ratefile.close()
 
+    # roll according to rate bounds
     bound = rates[8]
     seed = random.randrange(1,bound)
     resources = [0,0,0,0,0,0,0,0]
-    i = 0
 
+    i = 0
     for x in resources:
         resources[i] = str(seed * rates[i] + random.randrange(1,bound))
         i += 1
 
     return resources
+
+def sum_resources(reslist):
+    '''
+    Takes a given resource list and returns a sum of its total.
+    '''
+
+    total = 0
+    for x in res:
+        total += int(x)
+
+    return total
+
+## legacy functions below
 
 ## mine output
 
@@ -81,11 +232,6 @@ def getWorkers(mine): # return str list of contracted workers
 
     return workerList
 
-def sumRes(res): # returns total for a list of res
-    total = 0
-    for x in res:
-        total += int(x)
-    return total
 
 ## mine input
 
@@ -97,55 +243,6 @@ def writeMine(mine, minedata):
 
 ## mine actions
 
-def excavate(mine, rate=10, width=3):
-    excavated = [0,0,0,0,0,0,0,0]
-    res = getRes(mine)
-
-    mineData = openMine(mine)
-    veins = []
-
-    if getTotal(mine) <= rate: # clear the mine
-
-        excavated = getRes(mine)
-        mineData[0] = "0,0,0,0,0,0,0,0"
-        writeMine(mine, mineData)
-        return excavated
-
-    i = width
-    mineChoices = [0,1,2,3,4,5,6,7]
-    while i > 0: # pick veins
-        vein = random.choice(mineChoices)
-        veins.append(vein)
-        mineChoices.remove(vein)
-        i -= 1
-
-    i = rate
-    veinsLeft = width
-    while i > 0: # strike!
-        if veinsLeft > 0:
-            strike = random.choice(veins)
-        else:
-            break
-
-        if int(res[strike]) > excavated[strike]: # check for remaining res
-            excavated[strike] += 1
-            i -= 1
-        else: # stop striking empy vein
-            veins.remove(strike)
-            veinsLeft -= 1
-
-    i = 0
-    while i < 8: # clear out res
-       vein = int(res[i])
-       vein -= excavated[i]
-       res[i] = str(vein)
-       i += 1
-
-    j = ','
-    mineData[0] = j.join(res)
-    writeMine(mine, mineData)
-
-    return excavated
 
 ######## LINE OF DEATH
 
